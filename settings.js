d3649ebefdee275bd5deeb7b0f27fd1e32e2d416
Lahiru <paneljoker2000@gmail.com>
2025-09-10 16:12:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~6sBDmCTZ#m70zYmJFdVM-_14iiflAUvbx3-WR3IxrdqPcKjg2t6Q"
+global.sessionID = process.env.SESSIONID || "
 global.mongodburi = process.env.MONGODB_URI || "mongodb+srv://ketkshqAqjXygEF5:ketkshqAqjXygEF5@freeuri.dmkyoml.mongodb.net/polo-0721977999?retryWrites=true&w=majority&appName=freeuri"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "0753178060"
